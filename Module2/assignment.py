b9deb4039f49ff065deb80c8c03cb1b408efafaf
gprabhak123 <gauthamp123@gmail.com>
2024-08-27 16:52:50 -0700

    finished module

---
+++ Module2/assignment.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+K = 0.6
+max_r = 3
+
+initial = 0.5
+max_days = 1000 # change based on conditions
+
+rArray = np.linspace(0.1,2.99,1000)
+
+
+bounded = True
+x_final = np.zeros((1000,300))
+for inc in range(0, len(rArray)):
+    r = rArray[inc]
+    x = np.zeros(max_days)
+    x[0] = initial
+
+    for i in range(1, max_days):
+        if bounded:
+            x[i] = x[i-1] + r*(1-(x[i-1]/K))*x[i-1]
+
+
+    x_final[inc,:] = x[-300:]
+
+
+
+
+
+
+
+
+    
+# THE MODEL ^
+# ------------------------------------------
+# THE BEHAVIOR / THE OUTPUT ? 
+
+
+plt.figure(1)
+plt.plot(rArray, x_final, '.')
+plt.ylabel('Population')
+plt.xlabel('r')
+plt.show()
